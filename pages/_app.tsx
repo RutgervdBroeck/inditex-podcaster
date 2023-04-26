1a1631b700d134c64cfe923fb37cf0062e4f61ba
Rutger van den Broeck <rutgervdbroeck@gmail.com>
2023-04-26 10:51:37 +0200

    Initial commit and setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import styles from "@/styles/App.module.css";
+import { Inter } from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const inter = Inter({ subsets: ["latin"] });
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Podcaster</title>
+      <meta name="description" content="Podcaster assessment for Inditex" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <main className={`${styles.main} ${inter.className}`}>
+      <Component {...pageProps} />
+    </main>
+  </>
+);
+
+export default App;

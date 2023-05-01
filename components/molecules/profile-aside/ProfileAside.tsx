fbfecbde8f3e967efcfebb8c2e6b791d999b8a0c
Rutger van den Broeck <rutgervdbroeck@gmail.com>
2023-05-01 13:05:42 +0200

    Add profile aside component

---
+++ components/molecules/profile-aside/ProfileAside.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./ProfileAside.module.css";
+
+interface ProfileAsideProps {
+  imageSrc: string;
+  imageAlt?: string;
+  title: string;
+  author: string;
+  description: string;
+}
+
+const ProfileAside = ({
+  imageSrc,
+  imageAlt = "Profile image",
+  title,
+  author,
+  description,
+}: ProfileAsideProps) => (
+  <aside className={styles.aside}>
+    <Image src={imageSrc} alt={imageAlt} width={220} height={220} />
+    <div className={styles.block}>
+      <h3>{title}</h3>
+      <span>By {author}</span>
+    </div>
+    <div className={styles.block}>
+      <h4>Description:</h4>
+      <span>{description}</span>
+    </div>
+  </aside>
+);
+
+export default ProfileAside;
--- pages/podcast/[id].tsx
+++ pages/podcast/[id].tsx
@@ -1,13 +1,32 @@
+import ProfileAside from "@/components/molecules/profile-aside/ProfileAside";
 import styles from "@/styles/Podcast.module.css";
 import { GetServerSideProps } from "next";
 
+type PodcastEntry = {
+  artistId: number;
+  artistName: string;
+  collectionName: string;
+  artworkUrl600: string;
+  description: string;
+};
+
 interface PodcastPageProps {
-  podcastData: any;
+  podcastData: PodcastEntry[];
 }
 
 const PodcastPage = ({ podcastData }: PodcastPageProps) => {
-  console.log(podcastData);
-  return <div className={styles.podcast}></div>;
+  const asideData = podcastData[0];
+
+  return (
+    <div className={styles.podcast}>
+      <ProfileAside
+        imageSrc={asideData.artworkUrl600}
+        title={asideData.collectionName}
+        author={asideData.artistName}
+        description={asideData.description}
+      />
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
@@ -19,7 +38,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return {
     props: {
       // TODO: Maybe format this data to what is only required on the podcast to display.
-      podcastData: data,
+      podcastData: data.results,
     },
   };
 };
